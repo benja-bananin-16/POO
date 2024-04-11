342e8cb6fca435d9ad91975c825aa48bdf095d3b
VacSSJ <vicentearaya@liceovvh.cl>
2024-04-11 08:44:28 -0400

    git push

--- BUCLES/01_ejemplo/script.js
+++ BUCLES/01_ejemplo/script.js
@@ -1,4 +1,4 @@
-function contar(){
+function contarwhile(){
     let contador = 20;
     let array = [];
 
@@ -7,5 +7,16 @@ function contar(){
         contador--;
     }
     
-    document.getElementById('lista').innerHTML =`Números del 20 al 10 <br> <h2>${array}</h2>`;
+    document.getElementById('lista').innerHTML =`Números del 20 al 10 con While <br><h2>${array}</h2>`;
 }
+
+function contarFor(){
+    let array = [];
+
+    for (let i= 20; i >= 10; i--){
+        array.push(i);
+    }
+    
+    document.getElementById('lista').innerHTML =`Números del 20 al 10 con For <br><h2>${array}</h2>`;
+}
+
