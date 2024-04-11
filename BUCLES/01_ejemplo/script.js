27e27ac5ce0cbc8b2d6694a44ae4eac4d2662ba0
VacSSJ <vicentearaya@liceovvh.cl>
2024-04-11 08:34:50 -0400

    ok

--- BUCLES/01_ejemplo/script.js
+++ BUCLES/01_ejemplo/script.js
@@ -7,5 +7,5 @@ function contar(){
         contador--;
     }
     
-    document.getElementById('lista').innerHTML =array;
+    document.getElementById('lista').innerHTML =`Números del 20 al 10 <br> <h2>${array}</h2>`;
 }
