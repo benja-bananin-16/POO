94e628888f9a44860dee78859b9146d30e035869
VacSSJ <vicentearaya@liceovvh.cl>
2024-04-11 08:19:55 -0400

    ok

---
+++ BUCLES/01_ejemplo/script.js
@@ -0,0 +1,11 @@
+function contar(){
+    let contador = 20;
+    let array = [];
+
+    while (contador > 0){
+        array.push(contador);
+        contador--;
+    }
+    
+    document.getElementById('lista').innerHTML =array;
+}
