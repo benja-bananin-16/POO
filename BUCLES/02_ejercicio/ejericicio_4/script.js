cae869258cd756a7e805b79f490705492ce4475d
VacSSJ <vicentearaya@liceovvh.cl>
2024-04-15 10:11:08 -0400

    ok

--- BUCLES/02_ejercicio/ejericicio_4/script.js
+++ BUCLES/02_ejercicio/ejericicio_4/script.js
@@ -1,16 +1,16 @@
 function contarfor() {
-    let numeros = [];
+    let array = [];
     for (let i = 0; i < 5; i++) {
         let numero = parseInt(prompt(`Por favor ingrese el número ${i + 1}: `));
-        numeros.push(numero);
+        array.push(numero);
     }
 
-    let mayor = numeros[0];
-    for (let i = 1; i < numeros.length; i++) {
-        if (numeros[i] > mayor) {
-            mayor = numeros[i];
+    let mayor = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > mayor) {
+            mayor = array[i];
         }
     }
 
-    document.getElementById('lista').innerHTML = `los numeros son: <h2>${numeros}</h2> El número mayor es: <h2>${mayor}</h2>`;
+    document.getElementById('lista').innerHTML = `los numeros son: <h2>${array}</h2> El número mayor es: <h2>${mayor}</h2>`;
 }
