a0500746328be4a94762654a60da014a119ec1bd
VacSSJ <vicentearaya@liceovvh.cl>
2024-07-10 10:11:39 -0400

    ok

--- This_logica/01/script.js
+++ This_logica/01/script.js
@@ -1,18 +1,4 @@
-document.getElementById('agregar').addEventListener('click', function() {
-    addName(this);
-});
-
-function addName(button) {
-    const nameInput = button.previousElementSibling;
-    const nameList = document.getElementById('lista');
-
-    if (nameInput.value.trim() !== "") {
-        const listItem = document.createElement('li');
-        listItem.textContent = nameInput.value;
-        nameList.appendChild(listItem);
-        nameInput.value = "";
-        nameInput.focus();
-    } else {
-        alert('Por favor, ingrese un nombre.');
-    }
-}
+let clickCount = 0;
+document.getElementById('clicks').onclick = function() {
+    this.textContent = 'clicks: ' + (++clickCount);
+};
--- This_logica/02/script.js
+++ This_logica/02/script.js
@@ -1,4 +1,18 @@
-let clickCount = 0;
-document.getElementById('clicks').onclick = function() {
-    this.textContent = 'clicks: ' + (++clickCount);
-};
+document.getElementById('agregar').addEventListener('click', function() {
+    addName(this);
+});
+
+function addName(button) {
+    const nameInput = button.previousElementSibling;
+    const nameList = document.getElementById('lista');
+
+    if (nameInput.value.trim() !== "") {
+        const listItem = document.createElement('li');
+        listItem.textContent = nameInput.value;
+        nameList.appendChild(listItem);
+        nameInput.value = "";
+        nameInput.focus();
+    } else {
+        alert('Por favor, ingrese un nombre.');
+    }
+}
