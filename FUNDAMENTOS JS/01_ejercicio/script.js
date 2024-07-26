5491b66f641d84a824a11272e35e513d6a474e9e
benja-bananin-16 <benjaminfarias@liceovvh.cl>
2024-07-26 11:09:46 -0400

    OK

--- FUNDAMENTOS JS/01_ejercicio/script.js
+++ FUNDAMENTOS JS/01_ejercicio/script.js
@@ -1,4 +1,4 @@
-//ejercicios creacion de variables 
+//////ejercicios creacion de variables 
 
 //una variable
 var Global= "Global"
@@ -28,3 +28,76 @@ function sumar(num1, num2) {
 console.log("Variables:\ndecimal:" + numeroDecimal + "\ncadena de texto:" + cadenaTexto + "\nbooleano: " + booleano + "\narreglo : " + arreglo + "\nobjeto: " + JSON.stringify(objeto) + "\nindefonida" + variableIndefinida + "\nnulo:" + variableNula + "\nresultado de funcion:" +
 + sumar());
 console.log( sumar ())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                           
+
+function encontrarMayorMenor(array) {
+    if (array.length === 0) {
+        return "Array vacío";
+    }
+
+    let mayor = array[0];
+    let menor = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > mayor) {
+            mayor = array[i];
+        } else if (array[i] < menor) {
+            menor = array[i];
+        }
+    }
+
+    return {
+        mayor: mayor,
+        menor: menor
+    };
+};
+
+let resultado = encontrarMayorMenor([3, 5, 7, 2, 200, 33, 100, 3, 400]);
+console.log(resultado);
+alert(`Mayor: ${resultado.mayor}, Menor: ${resultado.menor}`); 
