bd8f1a5d27a5e2bdcdd9b4594e567ce4fb31b321
benja-bananin-16 <benjaminfarias@liceovvh.cl>
2024-10-17 08:54:13 -0300

    ok

--- FUNDAMENTOS JS/34_ejercicio/script.js
+++ FUNDAMENTOS JS/34_ejercicio/script.js
@@ -1,15 +1,22 @@
 let prev = ''; // Guarda el primer número
 let op = '';   // Guarda el operador
+let resultShown = false; // Para saber si se ha mostrado un resultado
 
 // Función cuando se presiona un número
 function press(num) {
     let display = document.getElementById('display');
 
-    // Comprobamos si el display muestra '0'
-    if (display.innerText === '0') {
-        display.innerText = num; // Reemplazamos '0' por el número presionado
+    // Si el resultado ya se mostró, reiniciamos el display
+    if (resultShown) {
+        display.innerText = num; // Reemplazamos el resultado con el nuevo número
+        resultShown = false; // Indicamos que ya no se está mostrando el resultado
     } else {
-        display.innerText += num; // Añadimos el número presionado al final
+        // Comprobamos si el display muestra '0'
+        if (display.innerText === '0') {
+            display.innerText = num; // Reemplazamos '0' por el número presionado
+        } else {
+            display.innerText += num; // Añadimos el número presionado al final
+        }
     }
 }
 
@@ -20,24 +27,26 @@ function setOP(operator) {
     // Si no hay un número previo, guardamos el número actual
     if (prev === '') {
         prev = display.innerText; // Guardamos lo que está en pantalla
-    } else {
-        calculate(); // Si ya hay un número previo, hacemos el cálculo
+    } else if (!resultShown) {
+        calculate(); // Si ya hay un número previo y no hemos mostrado el resultado, hacemos el cálculo
     }
 
     op = operator; // Guardamos el operador seleccionado
     display.innerText = '0'; // Reseteamos el display para el próximo número
+    resultShown = false; // Aseguramos que no se está mostrando un resultado
 }
 
 // Función para realizar el cálculo
 function calculate() {
     let display = document.getElementById('display');
-    let curr = display.innerText; // Obtenemos el número actual en el display
+    let curr = parseFloat(display.innerText); // Convertimos el número actual a float
     let result = 0; // Variable para el resultado
- 
+    prev = parseFloat(prev); // Convertimos el número previo a float
+
     // Usando solo switch para las operaciones
     switch (op) {
         case '+':
-            result = parseFloat(prev) + parseFloat(curr);
+            result = prev + curr;
             break;
         case '-':
             result = prev - curr;
@@ -53,6 +62,7 @@ function calculate() {
     display.innerText = result; // Muestra el resultado en el display
     prev = ''; // Resetea el número previo
     op = '';   // Resetea el operador
+    resultShown = true; // Indicamos que se mostró un resultado
 }
 
 // Función para limpiar la calculadora
@@ -60,125 +70,44 @@ function clr() {
     document.getElementById('display').innerText = '0'; // Resetea el display a 0
     prev = ''; // Resetea el número previo
     op = '';   // Resetea el operador
+    resultShown = false; // Indicamos que no se ha mostrado un resultado
 }
 
-// Evento para manejar pulsaciones de teclas
-document.onkeydown = function(event) {
-    switch (event.which) { // Utiliza 'event.which' para obtener el código de la tecla
-        case 48: // '0'
-            press(0);
+// Manejo de teclado
+document.onkeydown = function (event) {
+    let key = event.key;
+    switch (key) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            press(key);
             break;
-        case '1': // '1'
-            press(1);
-            break;
-        case 50: // '2'
-            press(2);
-            break;
-        case 51: // '3'
-            press(3);
-            break;
-        case 52: // '4'
-            press(4);
-            break;
-        case 53: // '5'
-            press(5);
-            break;
-        case 54: // '6'
-            press(6);
-            break;
-        case 55: // '7'
-            press(7);
-            break;
-        case 56: // '8'
-            press(8);
-            break;
-        case 57: // '9'
-            press(9);
-            break;
-        case 107: // '+' (numpad)
-        case 61:  // '+' (Shift + igual)
+        case '+':
             setOP('+');
             break;
-        case 109: // '-' (numpad)
-        case 54:  // '-' (Shift + 6)
+        case '-':
             setOP('-');
             break;
-        case 106: // '*' (numpad)
+        case '*':
             setOP('*');
             break;
-        case 111: // '/' (numpad)
+        case '/':
             setOP('/');
             break;
-        case 190: // '.' (numpad o punto)
-            press('.');
-            break;
-        case 13:  // 'Enter'
+        case 'Enter':
             calculate();
             break;
-        case 8:   // 'Backspace'
-        case 67:  // 'C' para limpiar
-        case 99:  // 'c' para limpiar
+        case 'Backspace':
+        case 'c':
+        case 'C':
             clr();
             break;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let previousInput = '';
-let operator = '';
-
-function press(num) {
-    let display = document.getElementById('display');
-    display.innerText = display.innerText === '0' ? num : display.innerText + num;
-}
-
-function setOP(op) {
-    let display = document.getElementById('display');
-    if (previousInput === '') previousInput = display.innerText;
-    else calculate();
-    operator = op;
-    display.innerText = '0';
-}
-
-function calculate() {
-    let display = document.getElementById('display');
-    let prev = parseInt(previousInput);
-    let curr = parseInt(display.innerText);
-    let result;
-
-    if (operator === '+') result = prev + curr;
-    else if (operator === '-') result = prev - curr;
-    else if (operator === '*') result = prev * curr;
-    else if (operator === '/') result = prev / curr;
-
-    display.innerText = result;
-    previousInput = '';
-    operator = '';
-}
-
-function clr() {
-    document.getElementById('display').innerText = '0';
-    previousInput = '';
-    operator = '';
-}*/
